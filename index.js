6d05515ce625734f49cac6e75f4975639e817a32
Thakor Hareshkumar Dasharathji <88961451+haresh244556@users.noreply.github.com>
2022-02-09 20:19:34 +0530

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-console.log("hello")
-
-const Calculator = require("./Calculator")
-
-Calculator.Addition(25,35)
-Calculator.Subtraction(75,7)
-Calculator.Multiplication(12,15)
-Calculator.Division(250,5)
