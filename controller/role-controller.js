c2b6709738fa7a39b8c5cbf06ff334bcde2d8a9d
Thakor Hareshkumar Dasharathji <haresh24june@gmail.com>
2022-02-16 21:28:45 +0530

    db insert

---
+++ controller/role-controller.js
@@ -0,0 +1,11 @@
+module.exports.addRole = function(req,res){
+    //db insert role
+    console.log(req.body.roleName);
+    res.json({
+        msg:"role added",
+        status:200,
+        data:req.body
+    })
+}
+
+//roleName
